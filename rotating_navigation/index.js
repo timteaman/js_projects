50e15391307195a755247d94d965c0f00e2b9443
Timur <157748152+timteaman@users.noreply.github.com>
2024-11-27 14:33:36 +0100

    Delete rotating_navigation directory

--- rotating_navigation/index.js
+++
@@ -1,9 +0,0 @@
-const hero = document.getElementById('hero');
-const burger = document.getElementById('burger');
-const nav = document.getElementById('nav');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('burger--active');
-  hero.classList.toggle('hero--active');
-  nav.classList.toggle('nav--active');
-});
